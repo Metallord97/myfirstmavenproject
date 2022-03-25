e7372372bb8341630f3c0dcc7171d49bc75061ae
enrico <ciarlaenrico@gmail.com>
2022-03-25 16:15:55 +0100

    prova CI

--- src/main/java/myfirstmavenproject/MavenHelloWorld.java
+++ src/main/java/myfirstmavenproject/MavenHelloWorld.java
@@ -4,7 +4,7 @@ public class MavenHelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Maven World!");
+		System.out.println("Hello Mavenn World!");
 	}
 
 }
